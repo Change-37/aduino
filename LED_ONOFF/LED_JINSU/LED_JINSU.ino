138ed477f6d54310763cc8b20f9ea92f845f5b3c
Change-37 <202110305@bssm.hs.kr>
2021-04-19 13:48:42 +0900

    With Taehyeon

--- LED_ONOFF/LED_JINSU/LED_JINSU.ino
+++ LED_ONOFF/LED_JINSU/LED_JINSU.ino
@@ -12,88 +12,20 @@ void setup() {
 }
 
 void loop() {
-  byte n;
-  for (byte num = 0; num>16; num++){    
-    if(num / 8){ 
-      n = num%8;
-      digitalWrite(A,HIGH);
-        if(n / 4){
-          n %= 4; 
-          digitalWrite(B,HIGH);
-          if(n / 2){ 
-            n %= 2;
-            digitalWrite(C,HIGH);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-          else{
-            n %= 2;
-            digitalWrite(C,LOW);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-        }
-        else{
-          n %= 4;
-          digitalWrite(B,LOW);
-          if(n / 2){ 
-            n %= 2;
-            digitalWrite(C,HIGH);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-          else{
-            n %= 2;
-            digitalWrite(C,LOW);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-        }
-      }
-      
-      
-      
-      
-      
-      else{
-        n = num % 8;
-        digitalWrite(A,LOW);
-        if(n / 4){
-          n %= 4;
-          digitalWrite(B,HIGH);
-          if(n / 2){
-            n %= 2;
-            digitalWrite(C,HIGH);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-          else{
-            n %= 2;
-            digitalWrite(C,LOW);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-        }
-        else{
-          n %= 4;
-          digitalWrite(B,LOW);
-          if(n / 2){
-            n %= 2;
-            digitalWrite(C,HIGH);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-          else{
-            n %= 2;
-            digitalWrite(C,LOW);
-            if(n / 1){digitalWrite(D,HIGH);}
-            else{digitalWrite(D,LOW);}
-          }
-        }
-      }
+  for (byte num = 0; num<16; num++){ 
+
+    if(num & 1) digitalWrite(A, HIGH);
+    else digitalWrite(A, LOW);
     
-
-
+    if(num & 2) digitalWrite(B, HIGH);
+    else digitalWrite(B, LOW);
+    
+    if(num & 4) digitalWrite(C, HIGH);
+    else digitalWrite(C, LOW);
+    
+    if(num & 8) digitalWrite(D, HIGH);
+    else digitalWrite(D, LOW);
+       
     delay(1000);
   }
 }
